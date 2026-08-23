781bb42ceccbcb4b01bb8cd8f768b418a9523418
Toshit Jain <jtoshit45@gmail.com>
2026-08-23 21:01:02 +0530

    Change groq Model and Add health endpoint

--- src/main/java/com/toshit/DevAnalyzer/controller/HealthController.java
+++ src/main/java/com/toshit/DevAnalyzer/controller/HealthController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class HealthController {
 
-    @GetMapping("/test")
-    public String test() {
+    @GetMapping("/health")
+    public String Health() {
         return "App is running 🚀";
     }
 }
